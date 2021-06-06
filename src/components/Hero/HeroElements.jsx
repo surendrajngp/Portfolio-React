7a25b7155ec29ba81a91927e466f2b1c4ad1fa6f
surendrajngp <surendraj454@gmail.com>
2021-06-06 10:00:04 +0530

    Hero commit

--- src/components/Hero/HeroElements.jsx
+++ src/components/Hero/HeroElements.jsx
@@ -33,3 +33,71 @@ export const Hello = styled.span`
   font-family: "Roboto", sans-serif;
   line-height: 1.625em;
 `;
+
+export const Hero = styled.h1`
+  font-size: 60px;
+  font-weight: 700;
+  font-family: "Poppins", sans-serif;
+  color: #222222;
+  line-height: 1.5;
+  font-weight: bold;
+`;
+
+export const HeroDomain = styled.div`
+  color: #e45447;
+  font-weight: 400;
+  font-size: 30px;
+  line-height: 42px;
+  margin-bottom: 30px;
+  display: flex;
+  white-space: pre;
+`;
+
+export const HeroDynamicDomain = styled.span`
+  position: relative;
+  display: flex;
+  align-items: center;
+
+  &::after {
+    content: "";
+    position: absolute;
+    left: calc(100% + 5px);
+    width: 2px;
+    height: 35px;
+    border-right: 3px solid #e45447;
+  }
+`;
+
+export const Overview = styled.p`
+  margin-bottom: 45px;
+  padding-right: 155px;
+  color: #777777;
+  font-size: 15px;
+  font-weight: 400;
+  line-height: 1.625em;
+`;
+
+export const ProfileDownload = styled.a`
+  cursor: pointer;
+  padding: 25px 43px;
+  border-radius: 30px;
+  background: #90acd1;
+  color: #fff;
+
+  text-transform: capitalize;
+  display: inline-block;
+  font-size: 16px;
+  font-weight: 400;
+  letter-spacing: 1px;
+  line-height: 0;
+  transition: color 0.4s linear;
+  position: relative;
+  z-index: 1;
+  border: 0;
+  overflow: hidden;
+  text-align: center;
+  white-space: nowrap;
+  vertical-align: middle;
+`;
+
+export const HeroImg = styled.img``;
--- src/components/Hero/HeroSection.jsx
+++ src/components/Hero/HeroSection.jsx
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
   HeroSectionContent,
   HeroContainer,
   Row,
   Col,
   Hello,
+  Hero,
+  HeroDomain,
+  HeroDynamicDomain,
+  Overview,
+  ProfileDownload,
 } from "./HeroElements";
+import { init } from "ityped";
 
 const HeroSection = () => {
+  const domains = [
+    "Computer Science Engineer.",
+    "Front-End Developer.",
+    "SQL Developer.",
+  ];
+
+  useEffect(() => {
+    const myElement = document.querySelector("#myElement");
+    init(myElement, {
+      showCursor: false,
+      strings: domains,
+      typeSpeed: 150,
+    });
+  });
+
   return (
     <HeroSectionContent>
       <HeroContainer className="container">
@@ -15,6 +36,15 @@ const HeroSection = () => {
           <Col>
             <div>
               <Hello>HEY! I AM</Hello>
+              <Hero>Surendra Jangid !</Hero>
+              <HeroDomain>
+                I'm a <HeroDynamicDomain id="myElement"></HeroDynamicDomain>
+              </HeroDomain>
+              <Overview>
+                Dorem ipsum dolor sitamet, consectetur adipiscing elit, sed do
+                eiusm tempor incididunt ulabore et dolore magna aliqua.
+              </Overview>
+              <ProfileDownload>Download</ProfileDownload>
             </div>
           </Col>
           <Col>
